501f8f1df008fabcc1838b60cfd6e8950a296239
Matt Reimer <matt@northarrowresearch.com>
2022-03-16 11:24:44 -0700

    fixing minor map problems

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,15 @@
  * It is technically possible to convert this to typescript but the debugging becomes much more complicated
  * so a decision was made not to pursue this.
  */
+// THIS FIX IS NECESSARY FOR PRODUCTION AND WEBPACK!!!!
+// https://docs.mapbox.com/mapbox-gl-js/api/#transpiling-v2
+// https://github.com/mapbox/mapbox-gl-js/issues/10173
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const mapboxgl = require('mapbox-gl/dist/mapbox-gl')
+
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
+// mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default
+
 exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions
 
--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -1,21 +1,9 @@
 import * as React from 'react'
 import PageWrapper from '../components/PageWrapper'
 import { PublicMapWrapped } from '@namcbugdb/react-public-map'
-
-// THIS FIX IS NECESSARY FOR PRODUCTION AND WEBPACK!!!!
-// https://docs.mapbox.com/mapbox-gl-js/api/#transpiling-v2
-// https://github.com/mapbox/mapbox-gl-js/issues/10173
-// import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 const NAMCMap: React.FC = () => {
-    console.log('TEST env', {
-        Something: 'something',
-        GATSBY_MAPBOX_TOKEN: process.env.GATSBY_MAPBOX_TOKEN,
-        GATSBY_PUBLICMAP_API: process.env.GATSBY_PUBLICMAP_API,
-        GATSBY_VERSION: process.env.GATSBY_VERSION
-    })
     return (
         <PageWrapper fullHeight>
             <div style={{ flex: 1, margin: '1rem' }}>
