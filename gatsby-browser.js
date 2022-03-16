c0e848ef870d5b42867f4c6e7ced24d5066eb92a
Matt Reimer <matt@northarrowresearch.com>
2022-03-16 12:00:42 -0700

    trying to exclude mapbox from transpilation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
 // gatsby-browser.js
 import React from 'react'
 import log from 'loglevel'
+// https://docs.mapbox.com/mapbox-gl-js/guides/install/#transpiling
+import mapboxgl from '!mapbox-gl'
 
 const { NODE_ENV } = process.env
 if (NODE_ENV === 'development') log.enableAll()
