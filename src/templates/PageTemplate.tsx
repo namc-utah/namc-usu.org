996afaf3cba5dbea6a8bee3b8405bc0948eaefb6
Matt Reimer <matt@northarrowresearch.com>
2024-08-09 11:42:30 -0700

    Updating packages

--- src/templates/PageTemplate.tsx
+++ src/templates/PageTemplate.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { graphql } from 'gatsby'
 import { html2material } from '../lib/rehype'
 import { Helmet } from 'react-helmet'
-import Page from '../components/Page'
-import Home from '../components/Home'
-import { PublicMapWrapped } from '@namcbugdb/react-public-map'
+// import Page from '../components/Page'
+// import Home from '../components/Home'
 import PageWrapper from '../components/PageWrapper'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
@@ -13,6 +12,8 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 // https://docs.mapbox.com/mapbox-gl-js/api/#transpiling-v2
 // https://github.com/mapbox/mapbox-gl-js/issues/10173
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
+import { Box } from '@mui/material'
+import { PublicMapWrapped } from '@namcbugdb/react-public-map'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default
 
@@ -38,13 +39,15 @@ const Template: React.FC<TemplateProps> = ({
 }: TemplateProps) => {
     const { page, site } = data // data.markdownRemark holds your post data
     const { fields, html, frontmatter } = page
-    const { siteMetadata } = site
+    // const { siteMetadata } = site
     /**
      * NOTICE THIS!!!!!
      * Here's where the raw HTML gets mapped to react components
      */
     const content = html2material(html)
     const pageTitle = frontmatter.title ? `${site.siteMetadata.title}: ${frontmatter.title}` : site.siteMetadata.title
+
+  
 
     return (
         <>
@@ -53,12 +56,12 @@ const Template: React.FC<TemplateProps> = ({
             </Helmet>
             {/* {fields.slug === '/index' ? <Home>{content}</Home> : <Page title={pageTitle}>{content}</Page>} */}
             <PageWrapper fullHeight>
-                <div style={{ flex: 1, margin: '1rem' }}>
-                    <PublicMapWrapped
-                        apiUrl={process.env.GATSBY_PUBLICMAP_API as string}
-                        mapboxToken={process.env.GATSBY_MAPBOX_TOKEN as string}
-                    />
-                </div>
+                <Box style={{ flex: 1, margin: '1rem' }}>
+                <PublicMapWrapped
+                    apiUrl={process.env.GATSBY_PUBLICMAP_API as string}
+                    mapboxToken={process.env.GATSBY_MAPBOX_TOKEN as string}
+                />
+                </Box>
             </PageWrapper>
         </>
     )
